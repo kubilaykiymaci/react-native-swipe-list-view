62d87a64f475874bca63b8a1d368194349c59a59
kubilaykiymaci <kubilay.kiymaci@akbank.com>
2019-08-17 15:57:03 +0300

    feat: has been added flow type and lint entegration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// Configuration shemastore : http://json.schemastore.org/prettierrc
+
+module.exports = {
+  semi: false, // Print semicolons
+  printWidth: 100, // The line length where Prettier will try wrap.
+  tabWidth: 2, // Number of spaces per indentation level.
+  singleQuote: false, // Use single quotes instead of double quotes.
+  trailingComma: "none", // Print trailing commas wherever possible when multi-line.
+  jsxBracketSameLine: false // Put > on the last line instead of at a new line.
+}
---
+++ components/assets/styles/index.js
@@ -0,0 +1,19 @@
+// @flow
+
+export const styles = {
+  hidden: {
+    zIndex: 1,
+    bottom: 0,
+    left: 0,
+    overflow: "hidden",
+    position: "absolute",
+    right: 0,
+    top: 0
+  },
+  flatListContainer: {
+    flex: 1
+  },
+  sectionListContainer: {
+    flex: 1
+  }
+}
